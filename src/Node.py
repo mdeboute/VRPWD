a4e1aedff2f35029baaa8dfb49c0bf5abad146cf
paulodebamako <paul.archip@free.fr>
2023-01-15 12:27:07 +0100

    modified comment (test)

--- src/Node.py
+++ src/Node.py
@@ -1,7 +1,7 @@
 class Node(object):
     def __init__(self, lat: float, lon: float, demand: int):
         assert isinstance(lat, float), "ERROR: lat must be a float!"
-        assert isinstance(lon, float), "ERROR: lon must be float!"
+        assert isinstance(lon, float), "ERROR: lon must be a float!"
         assert isinstance(lon, float), "ERROR: demand must be an int!"
         self.lat = lat
         self.lon = lon
