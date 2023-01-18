2f747d91e6635c99b7bef6ff6be285264985ebe6
paulodebamako <paul.archip@free.fr>
2023-01-18 13:13:43 +0100

    refactor

--- src/Edge.py
+++ src/Edge.py
@@ -1,22 +1,20 @@
 class Edge(object):
     def __init__(
         self,
-        command_number,
-        internal_id,
+        index,
         source,
         destination,
         length,
         speed,
-        oneway,
         osm_id,
         osm_type,
+        travel_time
     ):
-        self.command_number = command_number
-        self.internal_id = internal_id
+        self.index = index
         self.source = source
         self.destination = destination
         self.length = length
         self.speed = speed
-        self.oneway = oneway
         self.osm_id = osm_id
         self.osm_type = osm_type
+        self.travel_time=travel_time
--- src/Node.py
+++ src/Node.py
@@ -1,8 +1,10 @@
 class Node(object):
-    def __init__(self, lat: float, lon: float, demand: int):
+    def __init__(self, index: int, lat: float, lon: float, demand: float):
+        assert isinstance(index, int), "ERROR: idx must be an int!"
         assert isinstance(lat, float), "ERROR: lat must be a float!"
         assert isinstance(lon, float), "ERROR: lon must be a float!"
-        assert isinstance(lon, float), "ERROR: demand must be an int!"
+        assert isinstance(demand, float), "ERROR: lon must be a float!"
+        self.index=index
         self.lat = lat
         self.lon = lon
         self.demand = demand
