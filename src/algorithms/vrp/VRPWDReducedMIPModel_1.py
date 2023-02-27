6576a70b01f6d42fad04269156050f5369e821f3
Hirwa Shema Lin <linhirwa@gmail.com>
2023-02-27 13:51:32 +0100

    fix: Improved subtour constraint for Restricted Case 1 MIP

--- src/algorithms/vrp/VRPWDReducedMIPModel_1.py
+++ src/algorithms/vrp/VRPWDReducedMIPModel_1.py
@@ -211,36 +211,23 @@ class VRPWDReducedMIPModel_1:
                         for i, j in model._vars[0].keys()
                         if i in tour and j in tour
                     )
-                    for a, b in tour_routes:
-                        if 0 in tour:
-                            model.cbLazy(
-                                nt_demand * model._vars[0][a, b]
-                                <= nt_demand
-                                * gp.quicksum(
-                                    model._vars[0][i, j]
-                                    for i, j in model._vars[0].keys()
-                                    if i in tour and j not in tour
-                                )
-                                + gp.quicksum(
-                                    model._vars[1][i, j]
-                                    for i, j in model._vars[1].keys()
-                                    if i in tour and j not in tour
-                                )
-                                + gp.quicksum(
-                                    model._vars[2][i, j]
-                                    for i, j in model._vars[2].keys()
-                                    if i in tour and j not in tour
-                                )
-                            )
-                        else:
-                            model.cbLazy(
-                                model._vars[0][a, b]
-                                <= gp.quicksum(
-                                    model._vars[0][i, j]
-                                    for i, j in model._vars[0].keys()
-                                    if i in tour and j not in tour
-                                )
-                            )
+                    model.cbLazy(
+                        nt_demand * gp.quicksum(
+                            model._vars[0][i, j]
+                            for i, j in model._vars[0].keys()
+                            if i in tour and j not in tour
+                        )
+                        + gp.quicksum(
+                            model._vars[1][i, j]
+                            for i, j in model._vars[1].keys()
+                            if i in tour and j not in tour
+                        )
+                        + gp.quicksum(
+                            model._vars[2][i, j]
+                            for i, j in model._vars[2].keys()
+                            if i in tour and j not in tour
+                        ) >= nt_demand
+                    )
 
         # Given a tuplelist of edges, find the shortest subtour
         # Tour = all the nodes traveled and covered by the vehicle and its drones in a non disjoint path
